a57b3cd1d4b55ded3c671bfe5b76321168c70aa9
Higor <higorjugler@hotmail.com>
2024-02-27 10:20:06 -0300

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Higor Jugler on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
