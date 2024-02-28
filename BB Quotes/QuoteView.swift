279bd652b6c958eddbabafc4d27ac2cb23a21586
Higor <higorjugler@hotmail.com>
2024-02-28 09:34:21 -0300

    Nova tela para exibicao dos personagens

---
+++ BB Quotes/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  BB Quotes
+//
+//  Created by Higor Jugler on 2/28/24.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
---
+++ BB Quotes/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  BB Quotes
+//
+//  Created by Higor Jugler on 2/28/24.
+//
+
+import Foundation
